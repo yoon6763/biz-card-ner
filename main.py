b92abd8fed5bee86d0d2f55ee247e5017d9b669d
전윤호 / uknow <yoon6763@naver.com>
2025-12-29 15:23:00 +0900

    번호/이메일 정규표현식 적용

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hi")
+import re
+
+# Regex patterns
+PHONE_REGEX = re.compile(r'(?:\+82[-.\s]?)?01[016789][-.\s]?\d{3,4}[-.\s]?\d{4}')
+EMAIL_REGEX = re.compile(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}')
+
+def extract_regex_entities(text):
+    return {
+        "phones": [m.group() for m in PHONE_REGEX.finditer(text)],
+        "emails": [m.group() for m in EMAIL_REGEX.finditer(text)]
+    }
+
+
+if __name__ == "__main__":
+    ocr_text = """김철수
+    테스트컴퍼니
+    010-1234-5678
+    kimcs@test.com"""
+    entities = extract_regex_entities(ocr_text)
+    print(entities)
